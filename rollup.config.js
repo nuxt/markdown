69ecac1f9f52bcb63479ba4a7e3cecab66b3bcba
Jonas Galvez <jonasgalvez@gmail.com>
2019-08-11 01:12:49 -0300

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import path from 'path'
+import { readJSONSync } from 'fs-extra'
+import commonjsPlugin from 'rollup-plugin-commonjs'
+import autoExternalPlugin from 'rollup-plugin-auto-external'
+
+const rootDir = process.cwd()
+const input = 'src/index.js'
+
+const pkg = readJSONSync(path.resolve(rootDir, 'package.json'))
+const name = path.basename(pkg.name)
+
+export default {
+  input: path.resolve(rootDir, input),
+  output: {
+    dir: path.resolve(rootDir, 'dist'),
+    entryFileNames: `nuxt-${name}.js`,
+    chunkFileNames: `nuxt-${name}-[name].js`,
+    format: 'cjs',
+    preferConst: true
+  },
+  plugins: [
+    autoExternalPlugin(),
+    commonjsPlugin()
+  ]
+}
